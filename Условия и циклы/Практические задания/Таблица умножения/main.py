e26761cef00c249972539eafd9bd16476b22f8ce
zavtann <zavtann@gmail.com>
2023-10-22 15:59:51 +0300

    init

--- Условия и циклы/Практические задания/Таблица умножения/main.py
+++ Условия и циклы/Практические задания/Таблица умножения/main.py
@@ -1,1 +1,9 @@
-# TODO Распечатать таблицу умножения
+
+size = 9
+for i in range(2, size + 1):
+
+    for j in range(2, size + 1):
+        res = i * j
+        end = " " if j != size else ""
+        print(f'{res:2}', end=end)
+    print()
